37d7bf8133550c01dd91953537c0ae18c4d7d3cc
Eric Wallen <ericwallen@Erics-MacBook-Pro.local>
2017-07-07 10:27:55 -0600

    commit

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ var $installPhaser = $('#install-phaser');
 var $loadingSprites = $('#loading-sprites');
 var $anchorPoints = $('#anchor-points');
 var $scaling = $('#scaling');
-var $flipping = $('#flipping');
 var $rotating = $('#rotating');
 var $screenScaling = $('#screen-scaling');
+var $switchingItems = $('#switching-items');
+var $addingSound = $('#adding-sound');
 
-var navArray = [$home, $installPhaser, $loadingSprites, $anchorPoints, $scaling, $flipping, $rotating, $screenScaling]
+var navArray = [$home, $installPhaser, $loadingSprites, $anchorPoints, $scaling, $rotating, $screenScaling, $switchingItems, $addingSound]
 
 $installPhaser.hide();
 $loadingSprites.hide();
 $anchorPoints.hide();
 $scaling.hide();
-$flipping.hide();
 $rotating.hide();
 $screenScaling.hide();
+$switchingItems.hide();
+$addingSound.hide();
 
-console.log(navArray);
 
 document.getElementById('home-button').addEventListener('click', function(){
   navSwitch($home);
@@ -34,14 +35,17 @@ document.getElementById('anchor-points-button').addEventListener('click', functi
 document.getElementById('scaling-button').addEventListener('click', function(){
   navSwitch($scaling);
 });
-document.getElementById('flipping-button').addEventListener('click', function(){
-  navSwitch($flipping);
-});
-document.getElementById('rotating-button').addEventListener('click', function(){
-  navSwitch($rotating);
-});
+// document.getElementById('rotating-button').addEventListener('click', function(){
+//   navSwitch($rotating);
+// });
 document.getElementById('screen-scaling-button').addEventListener('click', function(){
   navSwitch($screenScaling);
+});
+document.getElementById('switching-items-button').addEventListener('click', function(){
+  navSwitch($switchingItems);
+});
+document.getElementById('adding-sound-button').addEventListener('click', function(){
+  navSwitch($addingSound);
 });
 
 function navSwitch (turnOn){
@@ -60,7 +64,7 @@ for (var i = 0; i < navArray.length; i++) {
 
 // var installServerCode="npm install http-server -g";
 // // npm unistall http-server -g
-// 
+//
 // var pre=document.createElement("pre");
 // pre.textContent=installServerCode;
 // pre.className="prettyprint";
